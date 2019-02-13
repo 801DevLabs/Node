d6c2dab2a4782a5da2cc9bad11017de841d4a96f
801DevLabs <801DevLabs@gmail.com>
2019-02-13 15:00:24 -0700

    updated node server

--- Understanding the Basics/Creating a Node Server/app.js
+++ Understanding the Basics/Creating a Node Server/app.js
@@ -1,7 +1,9 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   const url = req.url
+  const method = req.method
   if(url === '/'){
     res.write('<html>')
     res.write('<head>')
@@ -12,6 +14,12 @@ const server = http.createServer((req, res) => {
     res.write('<input type="text"><button type="submit">Send</button>')
     res.write('</form></body>')
     res.write('</html>')
+    return res.end()
+  }
+  if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.txt', 'DUMMY')
+    res.statusCode = 302
+    res.setHeader('Location', '/')
     return res.end()
   }
   console.log(req.url, req.method, req.headers)
