72f904a5a4b6772391048b70858918829cfd8a58
801DevLabs <801DevLabs@gmail.com>
2019-02-13 15:21:39 -0700

    blocking and non-blocking code

--- Understanding the Basics/Creating a Node Server/app.js
+++ Understanding the Basics/Creating a Node Server/app.js
@@ -25,11 +25,12 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString()
       const message = parsedBody.split('=')[1]
-      fs.writeFileSync('message.txt', message)
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+      })
     })
-    res.statusCode = 302
-    res.setHeader('Location', '/')
-    return res.end()
   }
   // process.exit()
   res.setHeader('Content-Type', 'text/html')
