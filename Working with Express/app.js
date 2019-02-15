342c66ec81e4e29f4ede289ba8c599442e4ef7cd
801DevLabs <801DevLabs@gmail.com>
2019-02-15 11:14:58 -0700

    added using a helper function for navigation

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -1,4 +1,5 @@
 // IMPORTS AND PACKAGES
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -18,8 +19,7 @@ app.use(shopRoutes)
 
 // 404 ERROR PAGE
 app.use((req, res, next) => {
-
-  res.status(404).send('<h1>Page not found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 // CREATE THE SERVER ON PORT 3000
